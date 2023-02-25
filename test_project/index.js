55fdc7cf4f230591adaaefcbfa007b0adecad659
Schubert <schubertgoh@gmail.com>
2023-02-25 14:34:45 +0800

    done login

--- test_project/index.js
+++ test_project/index.js
@@ -4,6 +4,11 @@ import "./loadEnvironment.js";
 import "express-async-errors";
 import user from "./controllers/user.controller.mjs";
 import item from "./controllers/item.controller.mjs";
+import User from "./models/user.model.mjs"
+import passport from "passport";
+import bodyParser from "body-parser";
+import LocalStrategy from "passport-local";
+import passportLocalMongoose from "passport-local-mongoose"
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -13,7 +18,22 @@ app.use(express.json());
 
 // Load  routes
 app.use("/user", user);
-app.use("/item", item)
+// app.use("/item", item)
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(require("express-session")({
+//     secret: "Rusty is a dog",
+//     resave: false,
+//     saveUninitialized: false
+// }));
+  
+app.use(passport.initialize());
+app.use(passport.session());
+  
+// passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
 
 // Global error handling
 app.use((err, _req, res, next) => {
