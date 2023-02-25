50acb4e28b29b3f1b49e5d60a68e19c6327f8c9e
Yanhong-Kwek <124876614+Yanhong-Kwek@users.noreply.github.com>
2023-02-25 12:17:18 +0800

    added Dashboard to home

--- test_client/src/pages/Home.jsx
+++ test_client/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { ProSidebarProvider } from "react-pro-sidebar";
 import Navbar from '../components/Navbar';
+import Dashboard from './dashboard';
 
 function Home() {
 
     return (
         <div>
-        <ProSidebarProvider>
+            <Dashboard/>
+             <ProSidebarProvider>
+             
             <Navbar/>
         </ProSidebarProvider>
         </div>
---
+++ test_client/src/pages/dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Dashboard() {
+
+    return (
+        <div>
+            <h1>Hello, world!</h1>
+        </div>
+    );
+
+}
+
+export default Dashboard;
