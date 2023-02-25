a668ac9aeadaf2c9e51aca653a7ea20bea5ba90a
changyu04 <126082025+changyu04@users.noreply.github.com>
2023-02-25 12:27:46 +0800

    added Add Page

--- test_client/src/pages/Home.jsx
+++ test_client/src/pages/Home.jsx
@@ -1,10 +1,13 @@
+import { Button } from '@mui/material';
 import React from 'react';
 import { ProSidebarProvider } from "react-pro-sidebar";
 import Navbar from '../components/Navbar';
 import PopUpButton from './PopUpButton';
-
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+    const navigate = useNavigate();
+
 
     return (
         <div>
@@ -12,6 +15,10 @@ function Home() {
         <ProSidebarProvider>
             <Navbar/>
         </ProSidebarProvider>
+        <Button onClick={() => navigate("/Add")}className='btn'>
+                Add Claim
+        </Button>
+            
         </div>
     );
 
